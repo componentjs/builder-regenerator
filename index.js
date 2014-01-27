2945f4adf14d7186af46949bd5af7492aaea7ee9
Jonathan Ong <jonathanrichardong@gmail.com>
2014-01-26 16:27:47 -0800

    cache regenerated files

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 var fs = require('fs');
 var regenerator = require('regenerator');
+var calculate = require('sse4_crc32').calculate;
 
 var re = /\bfunction\s*\*/;
+
+// cache hashes of strings so we don't regenerate
+// the same string over and over again
+// since regeneration is very slow.
+// uses crc32 to hash the strings.
+// if there are collisions, just restart the process.
+var cache = Object.create(null);
 
 module.exports = builder_regenerator;
 
 function builder_regenerator(options) {
+  options = options || {};
+
   return function builder_regenerator(file, done) {
     if (file.extension !== 'js') return done();
     file.read(function (err, string) {
       if (err) return done(err);
-      if (re.test(string)) file.string = regenerator(string, options);
+      if (!re.test(string)) return done();
+
+      var crc = calculate(string);
+      if (crc in cache) {
+        file.string = cache[crc];
+        done();
+        return;
+      }
+
+      cache[crc] =
+      file.string = regenerator(string, options);
       done();
     })
   }
